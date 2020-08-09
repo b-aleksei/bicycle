a2ae141a8424370b8b7ec58c4bd33021b3bc7766
Aleksei Bugaev <103301@mail.ru>
2020-08-09 12:34:24 +0300

    добваляет меню и форму

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-console.log(123);
+
+(function () {
+	// main menu opener
+	const navButton = document.querySelector('.nav__toggle');
+	const navMain = document.querySelector('.nav');
+	let invertExpanded = (el) => el.getAttribute('aria-expanded') === 'false' ? 'true' : 'false';
+
+
+	navMain.classList.remove('no-js');
+	navButton.addEventListener('click', function () {
+		navButton.setAttribute('aria-expanded', invertExpanded(navButton));
+		navMain.classList.toggle('nav--menu-active');
+	});
+
+})();
