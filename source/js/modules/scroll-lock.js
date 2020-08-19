05bee9232ca5cbc33f3f9b99af17c6426192470e
Aleksei Bugaev <103301@mail.ru>
2020-08-19 17:03:55 +0300

    упрощает код модалки

--- source/js/modules/scroll-lock.js
+++ source/js/modules/scroll-lock.js
@@ -1,19 +1,4 @@
-const body = document.body;
 
-const getScrollbarWidth = () => {
-  const outer = document.createElement('div');
-  outer.style.visibility = 'hidden';
-  outer.style.overflow = 'scroll';
-  body.append(outer);
-  const inner = document.createElement('div');
-  outer.append(inner);
-  const scrollbarWidth = (outer.offsetWidth - inner.offsetWidth);
-  outer.remove();
-  if (body.offsetHeight > window.innerHeight) {
-    return scrollbarWidth;
-  }
-  return 0;
-};
 
 const getBodyScrollTop = () => {
   return (
@@ -23,18 +8,5 @@ const getBodyScrollTop = () => {
   );
 };
 
-const disableScrolling = () => {
-  const scrollWidth = getScrollbarWidth();
-  body.setAttribute('style', `padding-right: ${scrollWidth}px;`);
-  body.dataset.scrollY = `${getBodyScrollTop()}`;
-  body.style.top = `-${body.dataset.scrollY}px`;
-  body.classList.add('body-lock');
-};
 
-const enableScrolling = () => {
-  body.removeAttribute('style');
-  body.classList.remove('body-lock');
-  window.scrollTo(0, +body.dataset.scrollY);
-};
-
-export {disableScrolling, enableScrolling};
+// export {disableScrolling, enableScrolling};
